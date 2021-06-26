8f75d87512c504cad9ab71d6de6ddc216c637026
Bassel Farhat <73551211+farhatbassel@users.noreply.github.com>
2021-06-26 11:46:13 +0200

    Update passwordGenerator.py
    I added some changes to make the code look cleaner. I added the string module, and I also removed the ratio since that didn't feel necessary. Using rand.sample was a much better approach to the project. It also made the code much easier to read and understand.
    
    I also added the int() to the input made life a lot easier

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,76 +1,48 @@
 import random as rand
+import string
 
-symbols = list(":,.~`/'[]=-+_()*&^%$#@!")
-letters = list("abcdefghijklmnopqrstuvwxyz")
-nums = list("0123456789")
+symbols = string.punctuation
+lowerLetters = string.ascii_lowercase
+upperLetters = string.ascii_uppercase
+letters = lowerLetters + upperLetters
+nums = string.digits
 passwordList = []
 
 while True:
     try:
-        passLength = input("How long do you want you password (8 as a minimum)?")
-        while int(passLength) < 8:
+        passLength = int(input("How long do you want you password (8 as a minimum)?"))
+        while passLength < 8:
             passLength = input(
                 "Password is too short, it would be easily hackable please insert a length higher than 8:")
-        if int(passLength) >= 8:
+        if passLength >= 8:
             break
     except ValueError:
         print("The input was not a valid integer.")
 
 while True:
     try:
-        passStrength = input("What do you want your password to consist of?\n Input 0 for only letters\n Input 1 for both upper and lower case letters and numbers\n Input 2 for all letter,symbols and numbers.")
-        while int(passStrength) > 2:
+        passStrength = int(input("What do you want your password to consist of?\n Input 0 for only upper and lower letters\n Input 1 for both upper and lower case letters and numbers\n Input 2 for all letter,symbols and numbers."))
+        while passStrength > 2:
             passStrength = input(
                 "Incorrect input. Please enter a number between 0 and 2")
-        if int(passStrength) <= 2:
+        if passStrength <= 2:
             break
     except ValueError:
         print("The input was not a valid integer.")
 
 def passwordGenerator(passStrength, passLength):
-    if int(passStrength) == 0:
-        for i in range(int(passLength)):
-            passwordList.append(rand.choice(letters))
+    if passStrength == 0:
+        password = rand.sample(letters, passLength)
 
-    if int(passStrength) == 1:
-        ratio = rand.randint(2, int(passLength)-2)
+    if passStrength == 1:
+        strength1 = letters+nums
+        password = rand.sample(strength1, passLength)
 
-        for i in range(ratio):
-            if rand.randint(1,2) == 1:
-                passwordList.append(rand.choice(letters).upper())
-            else:
-                passwordList.append(rand.choice(letters))
+    if passStrength == 2:
+        all = letters+nums+symbols
+        password = rand.sample(all, passLength)
 
-        for i in range(ratio, int(passLength)):
-            passwordList.append(rand.choice(nums))
-
-    if int(passStrength) == 2:
-        ratioNum = rand.randint(2, int(passLength) - 2)
-        ratioSym = rand.randint(2, int(passLength) - 2)
-
-        while ratioNum + ratioSym > int(passLength):
-            ratioNum = rand.randint(2, int(passLength) - 2)
-            ratioSym = rand.randint(2, int(passLength) - 2)
-
-        if ratioNum > ratioSym:
-            tmp = ratioSym
-            ratioSym = ratioNum
-            ratioLet = tmp
-
-        for i in range(ratioLet):
-            passwordList.append(rand.choice(nums))
-
-        for i in range(ratioLet, ratioSym):
-            passwordList.append(rand.choice(symbols))
-
-        for i in range(ratioSym, int(passLength)):
-            if rand.randint(1, 2) == 1:
-                passwordList.append(rand.choice(letters).upper())
-            else:
-                passwordList.append(rand.choice(letters))
-
-    rand.shuffle(passwordList)
-    password = "".join(passwordList)
+    password = "".join(password)
     return password
 
 print(passwordGenerator(passStrength, passLength))
